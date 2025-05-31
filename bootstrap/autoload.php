fbc0846e559237ac3786e3200bda1b12cd340c56
sikanderchoudhry <sikander.choudhry@gmail.com>
2025-05-31 14:17:44 +0500

    Fixes

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -1,11 +1,11 @@
 <?php
 
 // Define minimum supported PHP version
-define('AKAUNTING_PHP', '8.1.0');
+define('XKAUNTING_PHP', '8.1.0');
 
 // Check PHP version
-if (version_compare(PHP_VERSION, AKAUNTING_PHP, '<')) {
-    $message = 'Error: Ask your hosting provider to use PHP ' . AKAUNTING_PHP . ' or higher for HTTP, CLI, and php command.' . PHP_EOL . PHP_EOL . 'Current PHP version: ' . PHP_VERSION . PHP_EOL;
+if (version_compare(PHP_VERSION, XKAUNTING_PHP, '<')) {
+    $message = 'Error: Ask your hosting provider to use PHP ' . XKAUNTING_PHP . ' or higher for HTTP, CLI, and php command.' . PHP_EOL . PHP_EOL . 'Current PHP version: ' . PHP_VERSION . PHP_EOL;
 
     if (defined('STDOUT')) {
         fwrite(STDOUT, $message);
